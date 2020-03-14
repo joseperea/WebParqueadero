6ca8798795863eb28582c3e66f7090aaedf302c0
jose hernando perea <johevape03@hotmail.com>
2020-03-14 11:30:02 -0500

    Creacion de proyecto en visualStudio

---
+++ WebParqueadero/WebParqueadero/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebParqueadero.MvcApplication" Language="C#" %>
---
+++ WebParqueadero/WebParqueadero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebParqueadero.Startup))]
+namespace WebParqueadero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
